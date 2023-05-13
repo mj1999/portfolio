4b0bfc963802c8ab4796b899cd2321c0b327eb9c
mj1999 <mayank.joshi92@yahoo.com>
2023-05-13 14:03:00 +0530

    Add files via upload

---
+++ Resume/script.js
@@ -0,0 +1,54 @@
+function smoothScroll(element){
+    let currPos = window.pageYOffset; 
+    let endPos = document.getElementById(element.getAttribute("data-value")).offsetTop;
+    var scrollInterval= setInterval(function(){
+        if(currPos>=endPos){
+            clearInterval(scrollInterval);
+            return;
+        }
+        currPos+=30;
+        window.scrollBy(0,30);
+          
+      }); 
+}
+let items = document.getElementsByClassName("nav-items");
+for(let item of items)
+{
+    
+    item.addEventListener("click",function(){
+        smoothScroll(item);
+    });
+}
+function progressFiller(){
+    let skillBars = document.querySelectorAll(".skill-progress > div");
+    for(let skillProgress of skillBars)
+    {
+        let progress = skillProgress.getAttribute("data-value");
+        let currProgress = 0;
+        var progressFill = setInterval(function(){
+            if(currProgress>=progress)
+            {
+                clearInterval(progressFill);
+                return;
+            }
+            currProgress+=1;
+            skillProgress.style.width=currProgress+"%";
+        },10)
+    }
+}
+var flag=0;
+var skillContainer = document.querySelector("#skills > div");
+window.addEventListener("scroll",function(){
+    var coordinates = skillContainer.getBoundingClientRect();
+    if(flag==0 && window.innerHeight>=coordinates.top)
+    {
+        progressFiller();
+        flag=1;
+    }
+    else if(this.window.innerHeight<coordinates.top){
+        flag=0;
+    }
+});
+
+
+
