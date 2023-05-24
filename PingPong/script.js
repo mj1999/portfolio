566414f760fd8768d36442256772c54b9c635dd2
mj1999 <mayank.joshi92@yahoo.com>
2023-05-24 18:29:33 +0530

    Add files via upload

--- PingPong/script.js
+++ PingPong/script.js
@@ -1,9 +1,21 @@
+var velocity = 0.03;
+var lastTime = null;
+var score = document.getElementById('player-score');
+var startScreen = document.getElementById('startScreen');
+var startScreenScore = document.querySelector("#high-score>span");
+var highScore = localStorage.getItem('highScore');
+var gameStarted=false;
+startScreenScore.innerHTML = highScore;
+var hue = 200;
+// ----------------------------Ball Class----------------------------------------------------------------------------
 class Ball{
+    direction={x:0.75,y:0.5};
     constructor(ballElement)
     {
         this.ballElement = ballElement;
+        this.reset();
     }
-    velocity = 0.1;
+    
     get x(){
         return parseFloat(getComputedStyle(this.ballElement).getPropertyValue("--x"));
 
@@ -21,68 +33,109 @@ class Ball{
     reset(){
         this.x=50;
         this.y=50;
-        this.direction={x:0.75,y:0.5};
-        while(Math.abs(this.direction.x)<=0.2 || Math.abs(this.direction.x)>=0.9)
-        {
-            const heading = randomNumberBetween(0,2*Math.PI);
-            this.direction = {x:Math.cos(heading),y:Math.sin(heading)};
+        velocity = 0.03;
+        
+        this.direction={x:0,y:0};
+        while(Math.abs(this.direction.x)<=0.2 || Math.abs(this.direction.x)>=0.9)               // randomising initial ball movement after every game start, using a random number between 0 and 2*PI,
+        {                                                                                       // then fetching x direction and y direction from it using sin and cosin of that number
+            const heading = Math.random()*2*Math.PI;
+            this.direction={x:Math.cos(heading),y:Math.sin(heading)};
         }
+        
+        score.innerHTML=0;
     }
-    update(){
-        let heading = randomNumberBetween(0,2*Math.PI);
-        console.log(heading);
-        this.x+=Math.cos(heading);
-        this.y+=Math.sin(heading);
-
+    update(delta,paddleRects){
+        this.x+=this.direction.x * velocity * delta;                                            //making the ball move by changing the top and left position of the ball constantly in accordance to velocity and delta
+        this.y+=this.direction.y * velocity * delta;
+        if(this.y<=0 || this.y>=100)                                                            //collision with top and bottom
+        {
+            this.direction.y*=-1;
+        }
+        if(paddleRects.some(r=>collisionTrue(r,this.ballElement.getBoundingClientRect())))      // collision detection between ball and paddle and changing of direction for the ball 
+        {
+            this.direction.x*=-1;
+            velocity+=0.003;                                                                    // increasing velocity to increase ball speed after every touch with the paddles
+            let value = parseInt(score.innerHTML);
+            value++;
+            score.innerHTML=value;
+            if(currScore%2=='0')                                                                 // code for changing color after every 2 touches
+            {
+                hue+=20;
+                document.querySelector('*').style.setProperty('--hue',hue);
+            }
+        }
+        if(this.x<0 || this.x>100)
+        {
+            if(isHighScore)
+            {
+                window.alert("Congratulations you have achieved a new high score");
+                isHighScore=false;
+            }
+            location.reload();
+              
+        }
     }
 
 }
-function  randomNumberBetween(min,max)
-{
-    return (Math.random()*max-min)+min;
+// ----Paddle and movement related functions----------------------------------------------------------------------------
+
+function collisionTrue(rect1,rect2){                                                                                                
+    return rect1.left<= rect2.right && rect1.right>=rect2.left && rect1.top<=rect2.bottom && rect1.bottom>=rect2.top;                   //collision detection code for collision b/w ball and paddles
 }
 var ball = new Ball(document.getElementById("ball"));
 var paddles = document.getElementsByClassName("paddle");
 var upKey = false;
 var downKey = false;
-document.addEventListener("keypress",keyDown);
+document.addEventListener("keydown",keyDown);
 document.addEventListener("keyup",keyUp);
 function keyDown(e){
     if(e.key=='w')
     {
         upKey=true;
-        console.log("pressed W")
+     
     }
     else if(e.key=='s')
     {
         downKey=true;
-        console.log("pressed S")
+     
     }
 }
 function keyUp(e){
     if(e.key=='w')
     {
         upKey=false;
-        console.log("keyReleased W")
     }
     else if(e.key=='s')
     {
         downKey=false;
-        console.log("keyReleased S")
+      
     }
 }
-
-function gameLoop(){
-    if(upKey)
+// ----------Game Loop-------------------------------------------------------------------------------------------------
+var isHighScore = false;
+function gameLoop(time){
+    currScore = parseInt(score.innerHTML);
+    if(currScore>highScore||highScore==null)                                                                                //comparing score with high score and updating in local storage accordingly
     {
+        isHighScore=true;
+        localStorage.setItem('highScore',currScore);                                                                    
+    }
+   
+    var paddleRects=[];
+    for(let paddle of paddles)
+    {
+        paddleRects.push(paddle.getBoundingClientRect());
+    }
+    if(upKey)
+    {   
         for(let paddle of paddles)
         {
             let value = getComputedStyle(paddle).getPropertyValue("--position")
-            if(value<=5)
+            if(value<=5)                                                                                                //making sure paddle doesnt go out of the vh
             {
                 continue;
             }
-            paddle.style.setProperty("--position",value-0.5);
+            paddle.style.setProperty("--position",value-0.7);                                                           //movement of paddle upwards
         }
     }
     else if(downKey)
@@ -90,18 +143,33 @@ function gameLoop(){
         for(let paddle of paddles)
         {
             let value = parseFloat(getComputedStyle(paddle).getPropertyValue("--position"));
-            console.log(value);
-            if(value>=95)
+            if(value>=95)                                                                                               //making sure paddle doesnt go out of the vh
             {
-                console.log(value);
                 continue;
             }
-            paddle.style.setProperty("--position",value+0.5);
+            paddle.style.setProperty("--position",value+0.7);                                                           //movement of paddle downwards
         }
     }
-    // ball.update();
+    
+    if(lastTime!=null)
+    {
+        let delta = time-lastTime;
+        ball.update(delta,paddleRects);                                                                                     // delta being the difference between time of animation frame generation , so that we can take frame drops into consideration and move our ball according to animation frames
+    }
+    lastTime = time;
     requestAnimationFrame(gameLoop);
 }
-gameLoop();
+// -------StartGame function--------------------------------------------------------------------------------------------
+function  startGame(e){
+    
+    if(e.key=='Enter' && gameStarted==false){
+        gameStarted=true;
+        startScreen.style.opacity='0';
+        gameLoop();
+    }
+}
 
 
+document.addEventListener("keypress",startGame);
+
+// ---------------------------------------------------------------------------------------------------------------------
