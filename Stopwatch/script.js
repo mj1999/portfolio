0e648176a134f897f19e38622810f7dba9c4ff41
mj1999 <mayank.joshi92@yahoo.com>
2023-07-02 12:45:07 +0530

    Add files via upload

--- Stopwatch/script.js
+++ Stopwatch/script.js
@@ -8,15 +8,15 @@ const laps = document.querySelector("#laps");
 
 const rotate = function (el, ishrHand) {
   let degree = parseInt(
-    getComputedStyle(el).getPropertyValue("--deg").split(" ")[0]
+    getComputedStyle(el).getPropertyValue("--deg").split(" ")[0]                       //fetching integer part of deg variable in css, changing which allows us to move the clock hands.
   );
   degree += 6;
-  if (ishrHand) {
+  if (ishrHand) {                                                                      // for hr hand per 60 minutes hour hand needs to move 30deg.
     degree *= 5;
   }
   el.style.setProperty("--deg", degree + "deg");
 };
-const update = function (el) {
+const update = function (el) {                                                          // time update function, per function call time is being incremented by 1;
   let time = parseInt(el.getInnerHTML());
   time++;
   if (time % 60 == 0) {
@@ -29,12 +29,12 @@ const update = function (el) {
   }
 };
 let execute = true;
-function start() {
-  if (execute) {
+function start() {                                                                      //function for starting the timer
+  if (execute) {                                                                       
     interval1 = window.setInterval(() => {
-      rotate(secHand, false);
+      rotate(secHand, false);                                                           
       update(digiSecs);
-    }, 1000);
+    }, 1000);                                                                           
     interval2 = window.setInterval(() => {
       rotate(minHand, false);
       update(digiMins);
@@ -44,15 +44,15 @@ function start() {
       update(digiHrs);
     }, 60 * 60 * 1000);
   }
-  execute = false;
+  execute = false;                                                                       // to make sure function runs only once even upon clicking start multiple times        
 }
-function stop() {
+function stop() {                                                                       // function for stopping the timer
   window.clearInterval(interval1);
   window.clearInterval(interval2);
   window.clearInterval(interval3);
   execute = true;
 }
-function reset() {
+function reset() {                                                                      //function for reset 
   window.clearInterval(interval1);
   window.clearInterval(interval2);
   window.clearInterval(interval3);
@@ -60,17 +60,17 @@ function reset() {
   let degree = parseInt(
     getComputedStyle(element).getPropertyValue("--deg").split(" ")[0]
   );
-  degree = 0;
-  element.style.setProperty("--deg", degree + "deg");
+  degree = 0;                                                                           
+  element.style.setProperty("--deg", degree + "deg");                                   //deg variable again initialised to 0
   digiHrs.innerText = "00";
   digiMins.innerText = "00";
   digiSecs.innerText = "00";
   laps.innerHTML = "";
   execute = true;
 }
-var lapNumber = 1;
-function lap() {
-  const listItem = document.createElement("div");
+var lapNumber = 1;                                                                       
+function lap() {                                                                        // function for lap
+  const listItem = document.createElement("div");                                       // new lap element being added 
   listItem.innerHTML = `<span>Lap ${lapNumber}</span> <span>${
     digiHrs.getInnerHTML() +
     ":" +
